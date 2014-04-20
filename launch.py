9f6fc73bcd33374f504edfd9f4e93e3cf69231de
maxvitek <maxvitek@gmail.com>
2014-04-20 10:40:07 -0500

    fault tolerance in monitor

--- launch.py
+++ launch.py
@@ -6,12 +6,11 @@ from homespun.monitor import Wemo, Hue, Nest, Apex, Roomba
 
 logging.basicConfig(filename='homespun.log',level=logging.DEBUG)
 
-logging.info('Loading monitors')
-w = Wemo()
-h = Hue()
-n = Nest()
-a = Apex()
-r = Roomba()
+w = None
+h = None
+n = None
+a = None
+r = None
 
 logging.info('Starting monitoring service')
 mark_time = datetime.datetime(2014, 1, 1)  # make a Mark immediately
@@ -21,34 +20,68 @@ while True:
         logging.info('Mark! ' + str(datetime.datetime.now()))
         mark_time = datetime.datetime.utcnow()
         
-        logging.info('wemo')
-        try:
-            w.status()
-        except:
-            pass
+        if not w:
+            try:
+                w = Wemo()
+            except:
+                pass
+        if w:
+            logging.info('wemo')
+            try:
+                w.status()
+            except:
+                pass
 
-        logging.info('hue')
-        try:
-            h.status()
-        except:
-            pass
+        if not h:
+            try:
+                h = Hue()
+            except:
+                pass
 
-        logging.info('nest')
-        try:
-            n.status()
-        except:
-            pass
+        if h:
+            logging.info('hue')
+            try:
+                h.status()
+            except:
+                pass
 
-        logging.info('apex')
-        try:
-            a.status()
-        except:
-            pass
+        if not n:
+            try:
+                n = Nest()
+            except:
+                pass
 
-        logging.info('roomba')
-        try:
-            r.status()
-        except:
-            pass
+        if n:
+            logging.info('nest')
+            try:
+                n.status()
+            except:
+                pass
+
+        if not a:
+            try:
+                a = Apex()
+            except:
+                pass
+
+        if a:
+            logging.info('apex')
+            try:
+                a.status()
+            except:
+                pass
+
+        if not r:
+            try:
+                r = Roomba()
+            except:
+                pass
+
+        if r:
+            logging.info('roomba')
+            try:
+                r.status()
+            except:
+                pass
 
         logging.info('end Mark!')
